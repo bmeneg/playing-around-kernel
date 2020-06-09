698b8575d78c7eff7f5aea4058f89749eacacc2e
Bruno Meneguele <bmeneguele@gmail.com>
2020-06-09 17:02:02 -0300

    crypto/kernel: reorganize sync variant
    Bring the code closer to the async variant, thus the user can easily compare
    them both.

--- crypto/kernelspace/sync.c
+++ crypto/kernelspace/sync.c
@@ -1,23 +1,24 @@
 /*
- * Copyright (c) 2018 Bruno E. O. Meneguele <bmeneguele@gmail.com>
+ * Copyright (c) 2020 Bruno Meneguele <bmeneguele@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
  */
 
 /*
- * In this example we're going to use an unusual cipher as an example: salsa20.
- * I'm using it because I had a problem during my work related to this guy and
- * during the time I was testing it I figured that it would be a nice article to
- * write about crypto subsystem.
+ * In this example we're going to use an unusual cipher as an example:
+ * salsa20.  I'm using it because I had a problem during my work related to
+ * this guy and during the time I was testing it I figured that it would be a
+ * nice article to write about crypto subsystem.
  *
- * Salsa20 was originally writen over blkcipher (synchronous multi-block cipher)
- * layer, which some time ago was deprecated in behalf of skcipher (symetric key
- * cipher) calls. Although you can see "type: blkcipher" in /proc/crypto when
- * you load salsa20 module, we're going to use skcipher type calls. These new
- * calls indentify the original type of the selected cipher and within its logic
- * make the changes between skcipher and blkcipher when needed.
+ * Salsa20 was originally writen over blkcipher (synchronous multi-block
+ * cipher) layer, which some time ago was deprecated in behalf of skcipher
+ * (symetric key cipher) calls. Although you can see "type: blkcipher" in
+ * /proc/crypto when you load salsa20 module, we're going to use skcipher type
+ * calls. These new calls indentify the original type of the selected cipher
+ * and within its logic make the changes between skcipher and blkcipher when
+ * needed.
  */
 
 /* __init/exit, macros (MODULE_*) that initializes the module itself */
@@ -34,23 +35,27 @@
 /* Printing helper functions */
 #include "../utils.h"
 
-static int __init using_crypto_init(void)
+static int __init crypto_sync_init(void)
 {
 	int err;
+
 	struct crypto_skcipher *tfm;
 	struct skcipher_request *req;
 	struct scatterlist sg;
-	char buffer[6];
+
+	char plaintext[16] = {0};
+	char ciphertext[16] = {0};
 	/* We're going to use a zerod 128 bits key */
-	char key[16] = { 0 };
+	char key[16] = {0};
+
 	/* Initialization Vector */
 	char *iv;
-	unsigned int ivsize;
+	size_t ivsize;
 
 	PR_DEBUG("initializing module\n");
 
-	/* Check the existence of the cipher in the kernel (it might be a module
-	 * and it isn't loaded. */
+	/* Check the existence of the cipher in the kernel (it might be a
+	 * module and it isn't loaded. */
 	if (!crypto_has_skcipher("salsa20", 0, 0)) {
 		PR_ERROR("skcipher not found\n");
 		return -EINVAL;
@@ -59,13 +64,13 @@ static int __init using_crypto_init(void)
 	/* Allocate synchronous cipher handler.
 	 *
 	 * For generic implementation you can provide either the generic name
-	 * "salsa20" or the driver (specific) name "salsa20-generic", since the
-	 * generic has higher priority compared to the x86_64 instruction
+	 * "salsa20" or the driver (specific) name "salsa20-generic", since
+	 * the generic has higher priority compared to the x86_64 instruction
 	 * implementation "salsa20-asm".
 	 *
 	 * Also, cypher type will be left 0 since there isn't any other type
-	 * other than the default one for this cypher and the mask also will be
-	 * 0 since I don't want to use the asynchronous interface variant.
+	 * other than the default one for this cypher and the mask also will
+	 * be 0 since I don't want to use the asynchronous interface variant.
 	 */
 	tfm = crypto_alloc_skcipher("salsa20", 0, 0);
 	if (IS_ERR(tfm)) {
@@ -93,52 +98,59 @@ static int __init using_crypto_init(void)
 		err = -ENOMEM;
 		goto error0;
 	}
-	print_hex_dump(KERN_DEBUG, "iv: ", DUMP_PREFIX_NONE, 16, 1, iv, ivsize,
-		       false);
-
-	/* The word to be encrypted */
-	memcpy(&buffer, "aloha\0", 6);
-	/* TODO: explain scatter/gather lists, that has relation to DMA */
-	sg_init_one(&sg, buffer, 6);
+	print_hex_dump(KERN_DEBUG, "iv: ", DUMP_PREFIX_NONE, 16, 1, iv,
+		       ivsize, false);
 
 	/* Requests are objects that hold all information about a crypto
 	 * operation, from the tfm itself to the buffers and IV that will be
-	 * used in the enc/decryption operations. But it also holds information
-	 * about asynchronous calls to the crypto engine. If we have chosen
-	 * async calls instead of sync ones, we should also set the callback
-	 * function and some other flags in the request object in order to be
-	 * able to receive the output date from each operation finished. */
+	 * used in the enc/decryption operations. But it also holds
+	 * information about asynchronous calls to the crypto engine. If we
+	 * have chosen async calls instead of sync ones, we should also set
+	 * the callback function and some other flags in the request object in
+	 * order to be able to receive the output date from each operation
+	 * finished. */
 	req = skcipher_request_alloc(tfm, GFP_KERNEL);
 	if (!req) {
 		PR_ERROR("impossible to allocate skcipher request\n");
 		err = -ENOMEM;
 		goto error0;
 	}
-	skcipher_request_set_crypt(req, &sg, &sg, 6, iv);
+
+	/* The word to be encrypted */
+	/* TODO: explain scatter/gather lists, that has relation to DMA */
+	memcpy(plaintext, "aloha", 6);
+	sg_init_one(&sg, plaintext, 16);
+	skcipher_request_set_crypt(req, &sg, &sg, 16, iv);
 
 	print_hex_dump(KERN_DEBUG, "orig text: ", DUMP_PREFIX_NONE, 16, 1,
-		       buffer, 16, true);
+		       plaintext, 16, true);
 
-	/* Encrypt operation against "buffer" content */
+	/* Encrypt operation against "plaintext" content */
 	err = crypto_skcipher_encrypt(req);
 	if (err) {
 		PR_ERROR("could not encrypt data\n");
 		goto error1;
 	}
-	sg_copy_to_buffer(&sg, 1, &buffer, 6);
-	print_hex_dump(KERN_DEBUG, "encr text: ", DUMP_PREFIX_NONE, 16, 1,
-		       buffer, 16, true);
 
-	/* Decrypt operation against the new buffer (scatterlist that holds the
-	 * ciphered text). */
+	sg_copy_to_buffer(&sg, 1, ciphertext, 16);
+	print_hex_dump(KERN_DEBUG, "encr text: ", DUMP_PREFIX_NONE, 16, 1,
+		       ciphertext, 16, true);
+
+	/* Time to decrypt */
+	memset(plaintext, 0, 16);
+	sg_init_one(&sg, ciphertext, 16);
+
+	/* Decrypt operation against the new buffer (scatterlist that holds
+	 * the ciphered text). */
 	err = crypto_skcipher_decrypt(req);
 	if (err) {
 		PR_ERROR("could not decrypt data\n");
 		goto error1;
 	}
-	sg_copy_to_buffer(&sg, 1, &buffer, 6);
+
+	sg_copy_to_buffer(&sg, 1, plaintext, 16);
 	print_hex_dump(KERN_DEBUG, "decr text: ", DUMP_PREFIX_NONE, 16, 1,
-		       buffer, 16, true);
+		       plaintext, 16, true);
 error1:
 	skcipher_request_free(req);
 error0:
@@ -146,14 +158,14 @@ error0:
 	return err;
 }
 
-static void __exit using_crypto_exit(void)
+static void __exit crypto_sync_exit(void)
 {
 	PR_DEBUG("exiting module\n");
 }
 
-module_init(using_crypto_init);
-module_exit(using_crypto_exit);
+module_init(crypto_sync_init);
+module_exit(crypto_sync_exit);
 
-MODULE_AUTHOR("Bruno E. O. Meneguele <bmeneguele@gmail.com>");
+MODULE_AUTHOR("Bruno Meneguele <bmeneguele@gmail.com>");
 MODULE_DESCRIPTION("Testing kernel crypto api");
 MODULE_LICENSE("GPL");
