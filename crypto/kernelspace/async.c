279ec9b4dfa85851b56e60d6dab8255ca047e8a1
Bruno Meneguele <bmeneguele@gmail.com>
2020-06-09 17:00:18 -0300

    crypto/kernel: complete async implementation
    The async variant is now complete. Although it uses the async interface we
    force the code to wait the operation completion. It was done just to make the
    example easier.

--- crypto/kernelspace/async.c
+++ crypto/kernelspace/async.c
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2020 Bruno Meneguele <bmeneguele@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
  */
 
 /* __init/exit, macros (MODULE_*) that initializes the module itself */
@@ -31,39 +31,40 @@ void crypto_req_done(struct crypto_async_request *req, int err)
 	complete(&wait->completion);
 }
 
-static int __init using_crypto_init(void)
+static int __init crypto_async_init(void)
 {
 	int err;
+
 	struct crypto_skcipher *tfm;
 	struct skcipher_request *req;
-	struct crypto_wait wait;
 	struct scatterlist sg;
-	char buffer[6];
+	DECLARE_CRYPTO_WAIT(wait);
+
+	char plaintext[16] = {0};
+	char ciphertext[16] = {0};
 	/* We're going to use a zerod 128 bits key */
-	char key[16] = { 0 };
+	char key[16] = {0};
+
 	/* Initialization Vector */
 	char *iv;
-	unsigned int ivsize;
+	size_t ivsize;
 
 	PR_DEBUG("initializing module\n");
 
-	/* Check the existence of the cipher in the kernel (it might be a module
-	 * and it isn't loaded. */
-	if (!crypto_has_skcipher("cbc(aes)", 0, 0)) {
+	/* Check the existence of the cipher in the kernel (it might be a
+	 * module and it isn't loaded. */
+	if (!crypto_has_skcipher("salsa20", 0, 0)) {
 		PR_ERROR("skcipher not found\n");
 		return -EINVAL;
 	}
 
 	/* Allocate asynchronous cipher handler.
 	 *
-	 * For generic implementation you can provide either the generic
-	 * name "cbc(aes)" or the driver (specific) name "cbc-aes-aesni".
-	 *
-	 * Also, cypher type will be left 0 since we want the default
-	 * handler, but for the mask flag we're going to set the one that
-	 * allow us to use the asynchronous handler.
+	 * Cypher type will be left 0 since we want the default handler, but
+	 * for the mask flag we're going to set the one that allow us to use
+	 * the asynchronous handler.
 	 */
-	tfm = crypto_alloc_skcipher("cbc(aes)", 0, CRYPTO_ALG_ASYNC);
+	tfm = crypto_alloc_skcipher("salsa20", 0, CRYPTO_ALG_ASYNC);
 	if (IS_ERR(tfm)) {
 		PR_ERROR("impossible to allocate skcipher\n");
 		return PTR_ERR(tfm);
@@ -89,13 +90,8 @@ static int __init using_crypto_init(void)
 		err = -ENOMEM;
 		goto error0;
 	}
-	print_hex_dump(KERN_DEBUG, "iv: ", DUMP_PREFIX_NONE, 16, 1, iv, ivsize,
-		       false);
-
-	/* The word to be encrypted */
-	memcpy(&buffer, "aloha\0", 6);
-	/* TODO: explain scatter/gather lists, that has relation to DMA */
-	sg_init_one(&sg, buffer, 6);
+	print_hex_dump(KERN_DEBUG, "iv: ", DUMP_PREFIX_NONE, 16, 1, iv,
+		       ivsize, false);
 
 	/* Requests are objects that hold all information about a crypto
 	 * operation, from the tfm itself to the buffers and IV that will be
@@ -107,34 +103,49 @@ static int __init using_crypto_init(void)
 		err = -ENOMEM;
 		goto error0;
 	}
+
+	/* The word to be encrypted */
+	/* TODO: explain scatter/gather lists, that has relation to DMA */
+	memcpy(plaintext, "aloha", 6);
+	sg_init_one(&sg, plaintext, 16);
+
 	crypto_init_wait(&wait);
 	skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_SLEEP,
 				      crypto_req_done, &wait);
-	skcipher_request_set_crypt(req, &sg, &sg, 6, iv);
+	skcipher_request_set_crypt(req, &sg, &sg, 16, iv);
 
 	print_hex_dump(KERN_DEBUG, "orig text: ", DUMP_PREFIX_NONE, 16, 1,
-		       buffer, 16, true);
+		       plaintext, 16, true);
 
-	/* Encrypt operation against "buffer" content */
-	err = crypto_skcipher_encrypt(req);
+	/* Encrypt operation against "plaintext" content */
+	err = crypto_wait_req(crypto_skcipher_encrypt(req), &wait);
 	if (err) {
 		PR_ERROR("could not encrypt data\n");
 		goto error1;
 	}
-	sg_copy_to_buffer(&sg, 1, &buffer, 6);
+	sg_copy_to_buffer(&sg, 1, ciphertext, 16);
 	print_hex_dump(KERN_DEBUG, "encr text: ", DUMP_PREFIX_NONE, 16, 1,
-		       buffer, 16, true);
+		       ciphertext, 16, true);
 
-	/* Decrypt operation against the new buffer (scatterlist that holds the
-	 * ciphered text). */
-	err = crypto_skcipher_decrypt(req);
+	/* Decrypt operation against the new buffer (scatterlist that holds
+	 * the ciphered text). */
+	memset(plaintext, 0, 16);
+	sg_init_one(&sg, ciphertext, 16);
+
+	crypto_init_wait(&wait);
+	skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_SLEEP,
+				      crypto_req_done, &wait);
+	skcipher_request_set_crypt(req, &sg, &sg, 16, iv);
+
+	err = crypto_wait_req(crypto_skcipher_decrypt(req), &wait);
 	if (err) {
 		PR_ERROR("could not decrypt data\n");
 		goto error1;
 	}
-	sg_copy_to_buffer(&sg, 1, &buffer, 6);
+
+	sg_copy_to_buffer(&sg, 1, plaintext, 16);
 	print_hex_dump(KERN_DEBUG, "decr text: ", DUMP_PREFIX_NONE, 16, 1,
-		       buffer, 16, true);
+		       plaintext, 16, true);
 error1:
 	skcipher_request_free(req);
 error0:
@@ -142,14 +153,14 @@ error0:
 	return err;
 }
 
-static void __exit using_crypto_exit(void)
+static void __exit crypto_async_exit(void)
 {
 	PR_DEBUG("exiting module\n");
 }
 
-module_init(using_crypto_init);
-module_exit(using_crypto_exit);
+module_init(crypto_async_init);
+module_exit(crypto_async_exit);
 
-MODULE_AUTHOR("Bruno E. O. Meneguele <bmeneguele@gmail.com>");
+MODULE_AUTHOR("Bruno Meneguele <bmeneguele@gmail.com>");
 MODULE_DESCRIPTION("Testing kernel crypto api");
 MODULE_LICENSE("GPL");
